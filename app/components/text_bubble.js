d02188ef53025f4d404c37bbeed4306d511cd122
Nate Reiners <nate.reiners@gmail.com>
2017-01-31 11:59:42 -0800

    add components folder

---
+++ app/components/text_bubble.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+export default class textBubble extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Text>This is the text bubble!</Text>
+    );
+  }
+
+}
