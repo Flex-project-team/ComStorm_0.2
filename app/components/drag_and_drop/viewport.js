cf7e9cece1915f96e79e2a888364769890ffc9e4
Ken Lee <kzl5010@gmail.com>
2017-01-31 12:34:22 -0800

    viewport func

--- app/components/drag_and_drop/viewport.js
+++ app/components/drag_and_drop/viewport.js
@@ -7,3 +7,27 @@ import React,{
     Animated,
     Dimensions
 } from 'react-native';
+
+export default class Viewport extends Component{
+    render(){
+        return (
+            <View style={styles.mainContainer}>
+                <View style={styles.dropZone}>
+                    <Text style={styles.text}>Drop me here!</Text>
+                </View>
+
+                {this.renderDraggable()}
+            </View>
+        );
+    }
+
+    renderDraggable(){
+        return (
+            <View style={styles.draggableContainer}>
+                <Animated.View style={styles.circle}>
+                    <Text style={styles.text}>Drag me!</Text>
+                </Animated.View>
+            </View>
+        );
+    }
+}
