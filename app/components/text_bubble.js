98a2a93ccc7ae1beef9156c74d99e1ffd8102c7f
Nate Reiners <nate.reiners@gmail.com>
2017-01-31 18:40:56 -0800

    add multiline textbox

--- app/components/text_bubble.js
+++ app/components/text_bubble.js
@@ -1,11 +1,24 @@
 import TextField from 'react-native-md-textinput';
 import React, {Component} from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, StyleSheet, TextInput} from 'react-native';
 export default class TextBubble extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
   render() {
     return (
       <ScrollView>
-        <TextField label={'Name'} highlightColor={'#00BCD4'} />
+        <TextInput
+          multiline={true}
+          editable = {true}
+          maxLength = {100}
+          style={{height: 100, width: 300, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+          borderColor={"#000000"}
+          autoGrow={true}
+         />
       </ScrollView>
     );
   }
