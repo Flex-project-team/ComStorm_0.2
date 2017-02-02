ed904d17e32afd49ea2fd918eef1af732f2901e0
nandini0407 <you@example.com>
2017-02-01 18:16:51 -0800

    test sf_cartoonist_hand font

--- app/scenes/comic_scene.js
+++ app/scenes/comic_scene.js
@@ -22,7 +22,7 @@ export default class ComicScene extends Component {
           <View style={styles.nav}>
             <Image source={require('../images/logo.png')} style={{margin: 10 }}/>
           </View>
-          <Text style={{fontFamily: 'sf_cartoonist_hand_bold_italic'}}>
+          <Text style={{fontFamily: 'sf_cartoonist_hand'}}>
             testing
           </Text>
           <View style={styles.container}>
